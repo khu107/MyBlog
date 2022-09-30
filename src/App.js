18feae4f1b8e8668a2db61141fd7df563c5f237a
metablog <khu10735@naver.com>
2022-10-01 00:24:38 +0900

    modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [kiyim, setKiyim] = useState([
+    'erkak kiyim taklif',
+    'surxon oshhona',
+    'js urganish',
+  ]);
+  const [zur, setZur] = useState(0);
+  const [modal, setModal] = useState(false);
+
+  function zurr() {
+    setZur((e) => e + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="black-nav">
+        <h4>MyBlog</h4>
+      </div>
+      <div className="list">
+        <h4
+          onClick={() => {
+            setModal(!modal);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {kiyim[0]} <span onClick={() => zurr()}>👍</span> {zur}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{kiyim[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{kiyim[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      {modal ? <Modal /> : null}
     </div>
   );
+
+  function Modal() {
+    return (
+      <div className="modal">
+        <h4>mavzu</h4>
+        <p>chislo</p>
+        <p>oydinlik</p>
+      </div>
+    );
+  }
 }
 
 export default App;
